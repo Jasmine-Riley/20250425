860447b80802be831ef97f170ad0bef8023ecaae
cksuwjr <cksuwjr@naver.com>
2025-03-18 16:53:14 +0900

    인터페이스 정립

---
+++ Assets/Script/Player/Movement.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour, IMove
+{
+    public bool movable { get; protected set; }
+
+    public void Move(Vector3 direction)
+    {
+        
+    }
+}
